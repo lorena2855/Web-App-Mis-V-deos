15e79245eea1592942b45b1353f73650f15254cd
lorena2855 <61516883+lorena2855@users.noreply.github.com>
2020-05-22 04:26:44 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+
+    database: {
+        host: 'sql3.freemysqlhosting.net',
+        user: 'sql3341756',
+        password: 'BZ5jUaZIQp',
+        database: 'sql3341756'
+    }
+
+};
